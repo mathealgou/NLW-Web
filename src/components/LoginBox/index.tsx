62c4afe31d7bc45480ae2a2771a7db925e5391c6
Matheus Goulart <matheus.goulart@ozmap.com.br>
2021-10-22 16:41:55 -0300

    .

---
+++ src/components/LoginBox/index.tsx
@@ -0,0 +1,19 @@
+import { useContext, useEffect } from 'react'
+import { VscGithubInverted } from 'react-icons/vsc'
+import { api } from '../../services/api'
+import { AuthContext } from '../../context/auth'
+import styles from './styles.module.scss'
+
+export function LoginBox() {
+  const { signInUrl, user } = useContext(AuthContext)
+  //console.log(user)
+  return (
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe sua mensagem</strong>
+      <a href={signInUrl} className={styles.signInWithGithub}>
+        <VscGithubInverted size="24" />
+        Entrar com github
+      </a>
+    </div>
+  )
+}
